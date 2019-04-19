e8fff554312b89a3fd6ed726ed969f9f562b714b
guantemp <guantemp@126.com>
2019-04-19 20:27:13 +0800

    201904192023

---
+++ core/src/test/java/event/foxtail/AppTest.java
@@ -0,0 +1,18 @@
+package event.foxtail;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
